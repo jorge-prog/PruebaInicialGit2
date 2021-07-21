f98ed852be8d37c966c4f5bdd6cc4b26b47b8bc9
jorge <janus@192.168.1.138>
2021-07-21 09:44:44 +0200

    se añade resto

--- src/main/java/com/mycompany/pruebainicialparagit/ClasePrueba.java
+++ src/main/java/com/mycompany/pruebainicialparagit/ClasePrueba.java
@@ -11,15 +11,20 @@ package com.mycompany.pruebainicialparagit;
  */
 public class ClasePrueba {
     public int suma (int a, int b){
-    return a+b;
-}
+        return a+b;
+    }
     public int resta (int a, int b){
-    return a-b;
-}
+        return a-b;
+    }
     public int division (int a, int b){
-    return a/b;
+        return a/b;
     }
     public int multiplicacion (int a, int b){
-    return a*b;
-}
+        return a*b;
+    }
+    
+    public int resto (int a, int b){
+        return a%b;
+    }
+    
 }
