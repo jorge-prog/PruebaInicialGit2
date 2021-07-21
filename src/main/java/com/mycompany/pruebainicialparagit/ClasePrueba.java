56d7ec5c4d653f5f6a7b0fa07c9273eb3c965c13
jorge <janus@192.168.1.138>
2021-07-21 09:24:19 +0200

    creacion de proyecto classe

---
+++ src/main/java/com/mycompany/pruebainicialparagit/ClasePrueba.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pruebainicialparagit;
+
+/**
+ *
+ * @author janus
+ */
+public class ClasePrueba {
+    public int suma (int 1, int b){
+    return a+b;
+}
+}
